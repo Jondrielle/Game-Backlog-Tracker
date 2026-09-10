ed18d6e7d273a1ed9f416cb1fa88fbbb29dda6b5
Jondrielle <Jondriellewilson@gmail.com>
2026-09-09 20:52:35 -0500

    Working on schema model structure

--- backend/enum/enum.py
+++ backend/enum/enum.py
@@ -1,6 +1,5 @@
 from enum import Enum
 
-
 class Status(str, Enum):
 	BACKLOG = "Backlog"
     PLAYING = "Playing"
--- backend/schemas/game.py
+++ backend/schemas/game.py
@@ -1,16 +1,17 @@
 from pydantic import BaseModel,Field
-from date import Date 
+from datetime import date 
+from typing import Optional
 from enum import Platform,Status,Genre
 
 class GameBase(BaseModel):
 	name: str
 	status: Status
-	rating: int
-	notes:str = Field(max_length = 500)
+	rating: int | None = Field(default= None,ge=1,le=5)
+	notes:str | None = Field(default=None, max_length = 500)
 	platform: Platform
 	genre: Genre
-	release_year: Date
-	date_completed: Date
+	release_date: date
+	date_completed: date | None = None
 
 class Game(gameBase):
 	id: int
