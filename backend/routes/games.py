f84f973df3d0eb318833669c95fbec8f898900ab
Jondrielle <Jondriellewilson@gmail.com>
2026-09-14 15:15:23 -0500

    Added delete and patch endpoints

--- backend/routes/games.py
+++ backend/routes/games.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter
-from schemas.game import GameBase,CreateGame,Game
+from schemas.game import GameBase,CreateGame,Game,UpdateGame
+from enums.enum import Platform, Status, Genre
 
 game_router = APIRouter()
 
 counter_id = 0
 
+games = []
+
 @game_router.get("/")
 async def get_game():
-	return "Starting fastapi route connection"
+	return games
 
 @game_router.post("/",response_model = Game)
 async def create_game(game:CreateGame):
@@ -26,6 +29,43 @@ async def create_game(game:CreateGame):
 	)
 
 	counter_id += 1
+	games.append(new_game)
 
 	return new_game
 
+@game_router.delete("/game/{game_id}")
+async def delete_game(game_id: int):
+	for game in games:
+		if game.id == game_id:
+			games.remove(game)
+			return {"message": "Game Deleted Successfully"}
+
+@game_router.patch("/game/{game_id}", response_model = Game)
+async def update_game(game_id: int, updated_game: UpdateGame):
+	for game in games:
+		if game.id == game_id:
+			if updated_game.name is not None:
+				game.name = updated_game.name
+
+			if updated_game.status is not None:
+				game.status = updated_game.status
+
+			if updated_game.rating is not None:
+				game.rating = updated_game.rating
+
+			if updated_game.notes is not None:
+				game.notes = updated_game.notes
+
+			if updated_game.platform is not None:
+				game.platform = updated_game.platform 
+
+			if updated_game.genre is not None:
+				game.genre = updated_game.genre
+
+			if updated_game.release_date is not None:
+				game.release_date = updated_game.release_date
+
+			if updated_game.date_completed is not None:
+				game.date_completed = updated_game.date_completed
+
+			return game
--- backend/schemas/game.py
+++ backend/schemas/game.py
@@ -19,4 +19,14 @@ class Game(GameBase):
 class CreateGame(GameBase):
 	pass
 
+class UpdateGame(BaseModel):
+	name: Optional[str] = None
+	status: Optional[Status] = None
+	rating: Optional[int] = Field(default= None,ge=1,le=5)
+	notes:Optional[str] = Field(default=None, max_length = 500)
+	platform: Optional[Platform] = None
+	genre: Optional[Genre] = None
+	release_date: Optional[date] = None
+	date_completed: Optional[date] = None
+
 
