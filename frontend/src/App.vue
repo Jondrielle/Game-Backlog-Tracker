6b264518894ea09330e34b5207b4906b0006ffd1
Jondrielle <Jondriellewilson@gmail.com>
2026-09-17 23:11:58 -0500

    Adding logic for clearing list

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {ref} from 'vue'
+import GameItem from "./components/GameItem.vue"
 
 const games = ref([])
 
@@ -20,12 +21,88 @@ async function getGames(){
     console.error(err)
   }
 }
+
+async function getGame(id){
+  try{
+    const response = await fetch(`http://127.0.0.1:8000/game/${id}`)
+
+    if(!response.ok){
+      throw new Error(`Response Error:${response.status}`)
+    }
+
+    result = await response.json()
+
+    console.log(result)
+  }catch(err){
+    console.error(err)
+  }
+}
+
+async function deleteGame(id){
+  try{
+    const response = await fetch(`http://127.0.0.1:8000/game/${id}`,{
+        method:"DELETE"
+      }
+    )
+
+    if(!response.ok){
+      throw new Error(`Response Error:${response.status}`)
+    }
+
+    const result = await response.json()
+
+    games.value = games.value.filter(game => game.id != id)
+
+
+  }catch(err){
+    console.error(err)
+  }
+}
+
+async function clear(){
+  try{
+    const response = await fetch("http://127.0.0.1:8000/game",{
+      method:"DELETE"
+    })
+
+    if(!response.ok){
+      throw new Error(`Response Error: ${response.status}`)
+    }
+
+    console.log("List cleared")
+
+    getGames()
+  }catch(err){
+    console.error(err)
+  }
+}
+
+async function updateGame(){
+  
+}
 </script>
 
 <template>
   <h1>Game Backlog Tracker</h1>
 
   <button @click="getGames">Show List</button>
+
+  <button 
+    @click="getGame"
+  >Display Game</button>
+
+  <div
+      v-for="game in games"
+      :key="game.id"
+  >
+    {{game.name}}
+    <button 
+      @click="deleteGame(game.id)"
+    >Delete Game</button>
+  </div>
+
+  <button @click="clear">Clear List</button>
+
 </template>
 
 <style scoped></style>
