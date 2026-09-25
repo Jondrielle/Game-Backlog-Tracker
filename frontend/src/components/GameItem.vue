2266356808ea0c6fb07e33884e138b42e91dd094
Jondrielle <Jondriellewilson@gmail.com>
2026-09-24 22:51:59 -0500

    Moving specific game item fields into game item component

--- frontend/src/components/GameItem.vue
+++ frontend/src/components/GameItem.vue
@@ -1,9 +1,33 @@
 <script setup>
 
+const props = defineProps({
+	game: Object
+})
+
+const emit = defineEmits(['delete','edit'])
+
+function deleteGame(){
+	emit('delete',props.game.id)
+}
+
+function editGame(){
+	emit('edit',props.game)
+}
 </script>
 
 <template>
-<h1>Game Item</h1>
+<h1>{{game.name}}</h1>
+<div>
+	Status:{{game.status}}
+	Genre:{{game.genre}}
+	Rating:{{game.rating}}
+	Platform:{{game.platform}}
+	Notes:{{game.notes}}
+	Release Date:{{game.release_date}}
+	Game Completion:{{game.date_completed}}
+	-------------------
 
-//Display Game Stats name status genre rating platform notes release date, date completed
+	<button @click="deleteGame">Delete</button>
+	<button @click="editGame">Edit</button>
+</div>
 </template>
