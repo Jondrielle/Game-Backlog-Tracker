1635302b6670cb710c745d4dfcf039f5a17b1f31
Jondrielle <Jondriellewilson@gmail.com>
2026-09-24 22:52:54 -0500

    connecting componenets with app file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import {ref} from 'vue'
+import {ref,onMounted} from 'vue'
 import GameItem from "./components/GameItem.vue"
+import GameForm from "./components/GameForm.vue"
 
 //data
 const games = ref([])
@@ -49,6 +50,8 @@ async function getGames(){
       params.append("name",searchName.value)
     }
 
+    console.log(params.toString())
+    
     const response = await fetch(`http://127.0.0.1:8000/?${params.toString()}`)
 
     if(!response.ok){
@@ -60,6 +63,7 @@ async function getGames(){
     console.log(result)
 
     games.value = result
+
   }catch(err){
     console.error(err)
   }
@@ -103,7 +107,12 @@ async function addGame(game){
     })
 
     if(!response.ok){
-      throw new Error(`Response Error: ${response.status}`)
+      if (response.status === 422) {
+        throw new Error("Please check required fields.")
+      }
+
+      throw new Error("Failed to add game");
+
     }
 
     const result = await response.json()
@@ -197,6 +206,9 @@ async function updateGame(updatedGame){
     release_date: "",
     date_completed: null
   })
+
+  closeForm()
+
   }catch(err){
     console.error(err)
   }
@@ -232,113 +244,47 @@ function openForm(){
   showForm.value = true
 }
 
+function handleSubmit(game){
+  if(isEditing.value){
+    updateGame(game)
+  }else{
+    addGame(game)
+  }
+
+  closeForm()
+}
+
+onMounted(()=>{
+  getGames()
+})
+
 </script>
 
 <template>
   <h1>Game Backlog Tracker</h1>
 
+    <input v-model="searchName">
+    <button @click="getGames">Search</button>
 
-  <div
+    <GameItem
       v-for="game in games"
       :key="game.id"
-  >
-    Name:{{game.name}}
-    Status:{{game.status}}
-    Rating:{{game.rating}}
-    Notes:{{game.notes}}
-    Genre:{{game.genre}}
-    Platform: {{game.platform}}
-    Release Date: {{game.release_date}}
-    Completed: {{game.date_completed}}
-    <button 
-      @click="deleteGame(game.id)"
-    >Delete Game</button>
-    <button @click="startEdit(game)">Edit</button>
-  </div>
+      :game="game"
+      @edit="startEdit"
+      @delete="deleteGame"
+    />
+
   <button @click="clear">Clear List</button>
+
   <button @click="openForm">Open Add Form</button>
 
-  <form v-if="showForm" @submit.prevent="isEditing? updateGame(formGame):addGame(formGame)">
-  
-    <input
-      v-model="formGame.name"
-      type="text"
-      placeholder="Game title"
-    >
-
-    <select v-model="formGame.status">
-      <option disabled value="">Select status</option>
-      <option value="Backlog">Backlog</option>
-      <option value="Playing">Playing</option>
-      <option value="Completed">Completed</option>
-      <option value="Dropped">Dropped</option>
-    </select>
-
-    <select v-model="formGame.platform">
-      <option disabled value>Select Platform</option>
-      <option value="PC">PC</option>
-      <option value="PlayStation">PlayStation</option>
-      <option value="Xbox">Xbox</option>
-      <option value="Switch">Switch</option>
-    </select>
-
-    <select v-model="formGame.genre">
-      <option disabled value>Select genre</option>
-      
-      <option 
-      value="Action">Action</option>
-      
-      <option value="Adventure">Adventure</option>
-      
-      <option value="RPG">RPG</option>
-      
-      <option value="Strategy">Strategy</option>
-      
-      <option value="Simulation">Simulation</option>
-      
-      <option value="Sports">Sports</option>
-      
-      <option value="Racing">Racing</option>
-      
-      <option value="Puzzle">Puzzle</option>
-
-      <option value="Horror">Horror</option>
-      
-      <option value="Platformer">Platformer</option>
-      
-      <option value="Shooter">Shooter</option>
-
-      <option value="Fighting">Fighting</option>
-    </select>
-
-    <input
-      v-model="formGame.rating"
-      type="number"
-      min="1"
-      max="5"
-      placeholder="Rating"
-    >
-
-    <textarea
-      v-model="formGame.notes"
-      type="text"
-      placeholder="Notes"
-    ></textarea>
-
-    <input 
-      v-model="formGame.release_date"
-      type="date"
-    >
-
-    <input
-      v-model="formGame.date_completed"
-      type="date"
-    >
-
-    <button type="submit">{{ isEditing ? "Save Edit" : "Add Game"}}</button>
-
-    <button type="button" @click="closeForm">Cancel</button>
-  </form>
+  <GameForm
+    v-if="showForm"
+    :isEditing="isEditing"
+    :game="formGame"
+    @cancel="closeForm"
+    @submit="handleSubmit"
+  />
 
 </template>
 
